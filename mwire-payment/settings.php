63669a3a9750c7f0dd52e51370115a0abc7040cf
APex <37938428+APexBlockchainDevelopments@users.noreply.github.com>
2025-04-12 22:42:28 -0600

    updated version number

--- mwire-payment/settings.php
+++ mwire-payment/settings.php
@@ -6,7 +6,7 @@
  * @copyright 2025 Copyright(c) - All rights reserved.
  * @author    Austin Patkos / APex / mwire Development Team
  * @package   mwire-crypto-payments
- * @version   0.0.2
+ * @version   1.0.2
  */
 
 defined('ABSPATH') || exit;
